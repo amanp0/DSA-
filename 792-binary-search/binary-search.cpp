21d336eae5575cdaa161e194f5b91722a21bbd00
Aman Pandey <117337040+amanp0@users.noreply.github.com>
2025-08-01 20:31:01 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (79.74%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,25 +1,58 @@
+// class Solution {
+// public:
+
+//     bool binarySearch(int *nums, int s, int e, int key){
+        
+//         // base case
+
+//         //element not found
+//         if(s > e){
+//             return false;
+//         }
+        
+//         //element found 
+//         int mid = s + (e-s)/2;
+//         if(nums[mid] == key){
+//             return true;
+//         }
+//         if(nums[mid] < key){
+//             return binarySearch(nums, mid + 1, e, key);
+//         }
+//         else{
+//             return binarySearch(nums, s, mid-1, key);
+//         }
+//     }
+//     int search(vector<int>& nums, int target) {
+//         // using recursive function binarySearch
+
+//         bool found = binarySearch(nums.data(), 0, nums.size() - 1, target);
+//         return -1;
+
+//     }
+
+
+// };
+
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        //Binary search
-        
-        int n = nums.size();
-        int s = 0;
-        int e = n-1;
-        while(s<=e){
-            int mid = s + (e-s)/2;
-            if(nums[mid] == target){
-                return mid;
 
-            }
-            if(target > nums[mid]){
-                s = mid + 1;
-            }
-            else{
-                e = mid -1;
-            }
-           
+    int binarySearch(int *nums, int s, int e, int key){
+        if(s > e){
+            return -1;
         }
-         return -1;
+
+        int mid = s + (e - s) / 2;
+        if(nums[mid] == key){
+            return mid;
+        }
+        if(nums[mid] < key){
+            return binarySearch(nums, mid + 1, e, key);
+        } else {
+            return binarySearch(nums, s, mid - 1, key);
+        }
+    }
+
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums.data(), 0, nums.size() - 1, target);
     }
 };
