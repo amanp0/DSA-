cfbadea3714c3842a7bfb8789b1f9f09afe0bc73
Aman Pandey <117337040+amanp0@users.noreply.github.com>
2025-08-01 20:35:18 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (45.27%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,58 +1,33 @@
-// class Solution {
-// public:
-
-//     bool binarySearch(int *nums, int s, int e, int key){
-        
-//         // base case
-
-//         //element not found
-//         if(s > e){
-//             return false;
-//         }
-        
-//         //element found 
-//         int mid = s + (e-s)/2;
-//         if(nums[mid] == key){
-//             return true;
-//         }
-//         if(nums[mid] < key){
-//             return binarySearch(nums, mid + 1, e, key);
-//         }
-//         else{
-//             return binarySearch(nums, s, mid-1, key);
-//         }
-//     }
-//     int search(vector<int>& nums, int target) {
-//         // using recursive function binarySearch
-
-//         bool found = binarySearch(nums.data(), 0, nums.size() - 1, target);
-//         return -1;
-
-//     }
-
-
-// };
-
 class Solution {
 public:
 
     int binarySearch(int *nums, int s, int e, int key){
+        
+        // base case
+
+        //element not found
         if(s > e){
             return -1;
         }
-
-        int mid = s + (e - s) / 2;
+        
+        //element found 
+        int mid = s + (e-s)/2;
         if(nums[mid] == key){
             return mid;
         }
         if(nums[mid] < key){
             return binarySearch(nums, mid + 1, e, key);
-        } else {
-            return binarySearch(nums, s, mid - 1, key);
+        }
+        else{
+            return binarySearch(nums, s, mid-1, key);
         }
     }
-
     int search(vector<int>& nums, int target) {
+        // using recursive function binarySearch
+
         return binarySearch(nums.data(), 0, nums.size() - 1, target);
+
     }
+
+
 };
