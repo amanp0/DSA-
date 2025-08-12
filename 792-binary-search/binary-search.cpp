372741dcbd20ac19d568482270ea003475418ba5
Aman Pandey <117337040+amanp0@users.noreply.github.com>
2025-08-12 21:31:17 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (45.18%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,33 +1,12 @@
 class Solution {
 public:
-
-    int binarySearch(int *nums, int s, int e, int key){
-        
-        // base case
-
-        //element not found
-        if(s > e){
-            return -1;
-        }
-        
-        //element found 
-        int mid = s + (e-s)/2;
-        if(nums[mid] == key){
-            return mid;
-        }
-        if(nums[mid] < key){
-            return binarySearch(nums, mid + 1, e, key);
-        }
-        else{
-            return binarySearch(nums, s, mid-1, key);
-        }
-    }
     int search(vector<int>& nums, int target) {
-        // using recursive function binarySearch
-
-        return binarySearch(nums.data(), 0, nums.size() - 1, target);
-
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == target) {
+                return i; // return the index
+            }
+        }
+        return -1; // not found
     }
-
-
 };
