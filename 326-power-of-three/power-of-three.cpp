8905e2a5280a655bbaa256fa561ef09fa2c741fd
Aman Pandey <117337040+amanp0@users.noreply.github.com>
2025-08-13 23:03:25 +0530

    Time: 6 ms (27.44%) | Memory: 8.9 MB (76.82%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        //base case
-        if(n<=0){
-            return false;
-        }
-        if(n==1){
-            return true;
-        }
-        if(n%3!=0){
-            return false;
-        }
-        return isPowerOfThree(n/3);
+        return n > 0 && 1162261467 % n == 0;
     }
 };
