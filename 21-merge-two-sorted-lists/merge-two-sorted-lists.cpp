b780cafdc5a9553297ad19ce356e705b2186519d
Aman Pandey <117337040+amanp0@users.noreply.github.com>
2025-09-11 22:33:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (86.76%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,29 +10,54 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* dummy = new ListNode(-1);
-    ListNode* curr = dummy;
 
-    while (list1 != nullptr && list2 != nullptr) {
-        if (list1->val <= list2->val) {
-            curr->next = list1;
-            list1 = list1->next;
-        } else {
-            curr->next = list2;
-            list2 = list2->next;
+ListNode* solve(ListNode* list1, ListNode* list2){
+
+    if(list1 -> next == NULL){
+        list1 -> next = list2;
+        return list1;
+    }
+    ListNode* curr1 = list1;
+    ListNode* next1 = curr1 -> next;
+    ListNode* curr2 = list2;
+    ListNode* next2 = curr2 -> next;
+
+    while(next1 != NULL && curr2 != NULL){
+        if((curr2 -> val >= curr1 -> val) && (curr2 -> val <= next1 -> val)){
+            curr1 -> next = curr2;
+            next2 = curr2 -> next;
+            curr2 -> next = next1;
+
+            curr1 =curr2;
+            curr2 = next2;
         }
-        curr = curr->next;
-    }
+        else{
+            // curr 1 and next1 ange bhadana padega
+            curr1 = next1;
+            next1 = next1 -> next;
 
-    // Attach the remaining nodes
-    if (list1 != nullptr) {
-        curr->next = list1;
-    } else {
-        curr->next = list2;
+            if(next1 == NULL){
+                curr1 -> next = curr2;
+                //return list1;
+            }
+        }
     }
-
-    return dummy->next;
+    return list1;
 }
-    
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL){
+            return list2;
+        }
+        if(list2 == NULL){
+            return list1;
+        }
+
+        if(list1 -> val <= list2 -> val){
+           return solve(list1, list2);
+        }
+        else{
+            return solve(list2, list1);
+        }
+        return list1;
+    }
 };
