333ce3699f754ddee55386bb3477521461c4e5a8
Aman Pandey <117337040+amanp0@users.noreply.github.com>
2025-09-22 20:16:44 +0530

    Time: 6 ms (34.25%) | Memory: 121.7 MB (54.00%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -8,32 +8,59 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-#include<vector>
 class Solution {
 public:
-    bool checkPalindrome(vector<int> arr){
-        int n = arr.size();
-        int s = 0;
-        int e = n-1;
+    ListNode* getMid(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head -> next;
 
-        while(s<=e){
-            if(arr[s] != arr[e]){
-                return 0;
-            }
-            s++;
-            e--;
-        }       
-        return 1; 
+        while(fast != NULL && fast -> next != NULL){
+            fast = fast -> next -> next;
+            slow = slow -> next;
+        }
+        return slow;
+    }
+    ListNode* reverse(ListNode* head){
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+
+        while(curr != NULL){
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
     bool isPalindrome(ListNode* head) {
-        vector<int> arr;
-
-        ListNode* temp = head;
-        while(temp!=NULL){
-            arr.push_back(temp -> val);
-            temp = temp -> next;
+        if(head == NULL || head -> next == NULL){
+            return true;
         }
 
-        return checkPalindrome(arr);
+        //step 1 -> find middle
+        ListNode* middle = getMid(head);
+
+        //step 2 -> reverse list after middle
+        ListNode* temp = middle -> next;
+        middle -> next = reverse(temp);
+
+        //step 3 -> compare both half
+        ListNode* head1 = head;
+        ListNode* head2 = middle -> next;
+
+        while(head2 != NULL){
+            if(head1 -> val != head2 -> val){
+                return false;
+            }
+            head1 = head1 -> next;
+            head2 = head2 -> next;
+        }
+
+        //step 4 repead step 2nd
+        temp = middle -> next;
+        middle -> next = reverse(temp);
+
+        return true;
     }
 };
